d86d722e65ea9dc03e89e4cc2da0464359cc4dd4
Destin Saba <destinsaba9@gmail.com>
2024-05-20 09:27:35 -0600

    Minor touchups

--- input_processing.py
+++ input_processing.py
@@ -1,29 +1,20 @@
 # input_processing.py
 # DESTIN SABA, ENSF 692 P24
+
 # A terminal-based program for processing computer vision changes detected by a car.
 # Detailed specifications are provided via the Assignment 2 README file.
-# You must include the code provided below but you may delete the instructional comments.
-# You may add your own additional classes, functions, variables, etc. as long as they do not contradict the requirements (i.e. no global variables, etc.). 
-# You may import any modules from the standard Python library.
-# Remember to include your name and comments.
 
-
-
-# No global variables are permitted
-
-
-# You do not need to provide additional commenting above this class, just the user-defined functions within the class
 class Sensor:
 
-    # Must include a constructor that uses default values
-    # You do not need to provide commenting above the constructor
     def __init__(self):
         self.traffic_light = "green"
         self.pedestrian_status = "no"
         self.vehicle_status = "no"
 
-    # Replace these comments with your function commenting
-    def update_status(self): # You may decide how to implement the arguments for this function
+    # update_status() acts on the Sensor class. 
+    # It calls the get_input() function to ask the user for input for the traffic light, pedestrian, and vehicle status.
+    # It then processes the input, updates the Class instance variables and prints the result by calling the print_message() function.
+    def update_status(self): 
         choice = 1
         while(choice != 0):
             choice, change = get_input()
@@ -39,10 +30,12 @@ class Sensor:
             elif (choice ==4):
                 print_message(self)
 
+# The print_message() function accepts a sensor object to print the action message and current status.
+# Any scenario where a red light, a pedestrian or a vehicle are detected will display the message "STOP"
+# A green light with no pedestrian or vehicle detected will display the message "Proceed"
+# A yellow light with no pedestrian or vehicle detected will display the message "Caution"
+# After the action message, the current status of each monitored condition will be printed
 
-
-# The sensor object should be passed to this function to print the action message and current status
-# Replace these comments with your function commenting
 def print_message(sensor):
     print("")
     if ((sensor.traffic_light == "red") or (sensor.pedestrian_status == "yes") or (sensor.vehicle_status == "yes")):
@@ -56,10 +49,11 @@ def print_message(sensor):
     print("Light = " + sensor.traffic_light + " , Pedestrian = " + sensor.pedestrian_status + " , Vehicle = " + sensor.vehicle_status + " .")
     print("")
 
-#    If menu option 1, 2 or 3 are detected, the user should then be prompted to specify the detected change
-#    A traffic light can be "green", "yellow", or "red"
-#    Pedestrian status can be "yes" or "no"
-#    Vehicle status can be "yes" or "no"
+# get_input() creates the user interface that asks the user to input the detected status change
+# If menu option 1, 2 or 3 are detected, the user will be prompted to specify the detected change:
+#  * A traffic light can be "green", "yellow", or "red"
+#  * Pedestrian status can be "yes" or "no"
+#  * Vehicle status can be "yes" or "no"
 
 def get_input():
 
@@ -102,25 +96,14 @@ def get_input():
             print("You must select either 1, 2, 3, or 0.")
             print("")
 
+# Creates a Sensor object and runs the update_status() function.
 
-
-
-
-# Complete the main function below
 def main():
     status = Sensor()
     print("\n***ENSF 692 Car Vision Detector Processing Program***\n")
     status.update_status()
     exit()
 
-    
-
-
-
-
-
-# Conventional Python code for running main within a larger program
-# No additional code should be included below this
 if __name__ == '__main__':
     main()
 
